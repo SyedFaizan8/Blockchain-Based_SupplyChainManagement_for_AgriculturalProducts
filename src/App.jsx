cf25bf82dae49441cbb6114d6be784b145e07947
kirant2575@gmail.com <kirant2001@gmail.com>
2024-03-08 02:57:22 +0530

    added signin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Login from "./components/Login.jsx";
+import Signup from "./components/Signup.jsx";
 
 function App() {
   return (
     <>
       <Login />
+      <Signup/>
     </>
   );
 }
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const Login = () => {
+const Signup = () => {
   const [isChecked, setisChecked] = useState(true);
   const handlechange = () => {
     setisChecked(!isChecked);
@@ -15,18 +15,37 @@ const Login = () => {
       }}
       className="min-h-screen flex items-center justify-center w-full dark:bg-gray-950"
     >
-      <div className="bg-white dark:bg-gray-900 shadow-md rounded-lg px-8 py-6 max-w-md">
-        <h1 className="text-2xl font-bold text-center mb-4 dark:text-gray-200">
-          LOG IN
+      <div className="bg-white dark:bg-gray-900 shadow-md rounded-lg px-9 py-6 max-w-md">
+        <h1 className="text-3xl font-bold text-center mb-4 dark:text-yellow-500">
+         !Welcome to AgriGO
+         </h1>
+         <h1 className="text-2xl font-bold text-center mb-4 dark:text-gray-200">
+         SignUP
         </h1>
         <form>
           <div className="mb-4">
+          <label
+              htmlFor="email"
+              className="block text-sm font-medium text-stone-600-700 dark:text-gray-300 mb-2"
+            >
+              User Name
+            </label>
+            <div/>
+            <input
+              type="Name"
+              id="email"
+              className="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+              placeholder="your full name"
+              required
+            />
+            </div>
+            <div>
             <label
               htmlFor="email"
-              className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2"
-            >
+              className="block text-sm font-medium text-stone-600-700 dark:text-gray-300 mb-2" >
               Email Address
             </label>
+
             <input
               type="email"
               id="email"
@@ -53,11 +72,11 @@ const Login = () => {
               to="#"
               className="text-xs text-gray-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              Forgot Password?
             </a>
           </div>
           <div className="flex items-center justify-between mb-4">
             <div className="flex items-center">
+
               <input
                 type="checkbox"
                 id="remember"
@@ -72,19 +91,14 @@ const Login = () => {
                 Remember me
               </label>
             </div>
-            <label
-              href="#"
-              className="text-xs text-indigo-500 hover:text-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Create Account
-            </label>
+            
           </div>
           <button
             type="submit"
             className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white
              bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
-            Login
+            Sign
           </button>
         </form>
       </div>
@@ -92,4 +106,4 @@ const Login = () => {
   );
 }
 
-export default Login;
+export default Signup;
