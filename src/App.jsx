8c190c99ff92bea5caab16239370c8c06f55ad20
Faizan <syedfaizan.edu@gmail.com>
2024-03-15 17:36:55 +0530

    added shadcn login/signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-
-import Navbar from "./components/Navbar.jsx";
+import Login from "./components/Login.jsx";
 
 function App() {
   return (
     <>
-    <Navbar/>
+      <Login />
     </>
   );
 }
