f64c4858c9cf975272f508e483872756b3a2618d
Faizan <syedfaizan.edu@gmail.com>
2024-03-16 16:32:57 +0530

    cleared some changes of routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import Login from "./components/Login.jsx";
+// import { useSate } from "react";
+// import { useDispatch } from "react-redux";
+import Footer from "./components/Footer/Footer.jsx";
+import Header from "./components/Header/Header.jsx";
+// import HeaderSample from "./components/Header/HeaderSample.jsx";
+// import Login from "./components/Login.jsx";
+import { Outlet } from "react-router-dom";
 
 function App() {
+  // const [loading, setLoading] = useSate(true);
+  // const dispatch = useDispatch;
+
   return (
     <>
-      <Login />
+      <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+        <div className="w-full block">
+          <Header />
+          <main>
+            TODO: <Outlet />
+          </main>
+          <Footer />
+        </div>
+      </div>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Post from "./pages/Post.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/post/:slug",
+        element: <Post />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
